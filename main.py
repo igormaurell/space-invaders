12ffbc526403a998bf9a00d166a2662c63faf0b1
RobersonCosta <31294540+RobersonCosta@users.noreply.github.com>
2017-09-03 19:41:18 -0300

    Menu principal, porem sem os records

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 
+from game.menu import *
 from game import *
 
 def main():
@@ -10,10 +11,14 @@ def main():
 
     screen_size = (WIDTH, HEIGHT)
     screen = pygame.display.set_mode(screen_size)
-
     pygame.display.set_caption("Space Invaders")
-
-    stage = Stage('stage3', screen, 60)
-    stage.start()
+    menu = GameMenu('Space Invaders', screen, 60)
+    sequence = menu.run()
+    if sequence == 1:
+        stage = Stage('stage3', screen, 60)
+        stage.start()
+    else:
+        print("Records aqui!")
+        #SHOW RECORDS!!!
 
 main()
