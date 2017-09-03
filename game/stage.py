e273095e05494cb7706245263c7601311c4356e6
LucasVianaa <lucas.viana.rs@hotmail.com>
2017-09-03 20:33:36 -0300

    Transição de stage rudimentar

--- game/stage.py
+++ game/stage.py
@@ -1,28 +1,32 @@
 import pygame
 from pygame.locals import *
-from pygame.sprite import Group, GroupSingle, groupcollide, spritecollide
+from pygame.sprite import Group, groupcollide, spritecollide
 
 from random import randint
 
 from game import *
 import json
 
+MAX_STAGE = 3
+
 class Stage:
-    def __init__(self, key, screen, fps = 50):
+    def __init__(self, screen, fps = 60, key = 1):
         self.key = key
         self.json_data = None
         self.screen = screen
+        self.map = None
+        self.temp_effects = []
         self.bg = None
         self.FPS = fps
         self.done = False
-        self.CLOCK = None
+        self.CLOCK = pygame.time.Clock()
         
         self.player = None
         self.monsters = Group()
     
     def start(self):
         config = json.loads(open('definitions/stages.json').read())
-        config = config[self.key]
+        config = config['stage' + str(self.key)]
 
         self.bg = pygame.image.load(config['background']).convert_alpha()
         self.screen.blit(self.bg, (0, 0))
@@ -33,7 +37,7 @@ class Stage:
         self.player = Player('player', tuple(config['player_position']),
         conf['shot'], conf['shot_speed'], conf['life'], conf['demage'])
      
-        y = config['monsters_position'][1]
+        y = config['monsters_position'][1] - 180
         for i in range(1, 5):
             line = config['line' + str(i)]
             x = config['monsters_position'][0]
@@ -47,16 +51,26 @@ class Stage:
                     y_M = conf['size'][1]
             y += y_M
 
+        yMovToPos = 0
+        while True:
+            if yMovToPos < 100:
+                for monster in self.monsters:
+                    monster.setSpeed((0,1))
+                    monster.do()
+                yMovToPos += 1
+            else:
+                break
+            self.renderObjects()
+            self.CLOCK.tick(self.FPS)
+
         self.run()
 
     def run(self):
         framesSinceLastEnemyShot = 0
         lastMove = "right"
 
-        self.CLOCK = pygame.time.Clock()
-        temp_effects = []
-
         while not self.done:
+                
                 #Keyboard press and exit events
             for event in pygame.event.get():
                 if event.type == QUIT:
@@ -103,7 +117,7 @@ class Stage:
                     if value.life == 0:
                         self.player.score += value.value
                     collision_pos = value.getPosition()
-                    temp_effects.append(TempEffect("hit_blue", "effects", collision_pos))
+                    self.temp_effects.append(TempEffect("hit_blue", "effects", collision_pos))
 
             #Monsters-Shots collision with player
             for monster in self.monsters:
@@ -111,7 +125,7 @@ class Stage:
                 if(len(collided)):
                     self.player.life -= monster.demage
                     collision_pos = self.player.getPosition()
-                    temp_effects.append(TempEffect("hit_blue", "effects", collision_pos))
+                    self.temp_effects.append(TempEffect("hit_blue", "effects", collision_pos))
 
             #Select random enemy to shot and control time between shots
             indexMonShooting = randint(0, len(self.monsters))
@@ -125,33 +139,7 @@ class Stage:
                 framesSinceLastEnemyShot += 1
 
             #Updating and rendering objects
-
-            map = pygame.sprite.RenderUpdates()
-            
-            self.screen.blit(self.bg, (0, 0))
-
-            map.update()
-            map.draw(self.screen)
-
-
-            self.player.update()
-            self.player.update_attack_clock(self.CLOCK)
-            if(self.player.life > 0):
-                self.player.draw(self.screen)
-
-
-            for monster in self.monsters:
-                monster.update()
-                monster.draw(self.screen)
-
-            for i, tmp in enumerate(temp_effects):
-                if(tmp.is_dead()):
-                    temp_effects.pop(i)
-                else:
-                    tmp.update_time(self.CLOCK)
-                    tmp.draw(self.screen)
-
-            pygame.display.flip()
+            self.renderObjects()
 
             if self.player.life <= 0:
                 print ("Perdeu! SCORE: " + str(self.player.score))
@@ -162,3 +150,39 @@ class Stage:
 
             self.CLOCK.tick(self.FPS)
 
+        if(len(self.monsters) <= 0):
+            if(self.key < MAX_STAGE):
+                self.done = False
+                self.key += 1
+                self.start()
+            else:
+                print ("Ganhou! SCORE: " + str(self.player.score))
+
+    
+    def renderObjects(self):
+        self.map = pygame.sprite.RenderUpdates()
+        
+        self.screen.blit(self.bg, (0, 0))
+
+        self.map.update()
+        self.map.draw(self.screen)
+
+
+        self.player.update()
+        self.player.update_attack_clock(self.CLOCK)
+        if(self.player.life > 0):
+            self.player.draw(self.screen)
+
+
+        for monster in self.monsters:
+            monster.update()
+            monster.draw(self.screen)
+
+        for i, tmp in enumerate(self.temp_effects):
+            if(tmp.is_dead()):
+                self.temp_effects.pop(i)
+            else:
+                tmp.update_time(self.CLOCK)
+                tmp.draw(self.screen)
+
+        pygame.display.flip()
--- main.py
+++ main.py
@@ -15,7 +15,7 @@ def main():
     menu = GameMenu('Space Invaders', screen, 60)
     sequence = menu.run()
     if sequence == 1:
-        stage = Stage('stage3', screen, 60)
+        stage = Stage(screen, 60)
         stage.start()
     else:
         print("Records aqui!")
